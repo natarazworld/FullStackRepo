8f046afc97c35534d328b31a1758e860816be1dd
Nareshit <Nareshit@DESKTOP-IUDAAVL>
2021-06-27 14:23:51 +0530

    commit

--- Prij1/src/com/nt/comp/Payment.java
+++ Prij1/src/com/nt/comp/Payment.java
@@ -5,14 +5,10 @@ public class Payment {
 	public String doPayment(int acno,double amt) {
 		return "payment is done";
 	}
-<<<<<<< HEAD
 	
 	//hello1
 		public void m1() {
 			System.out.println("m1()");
 		}
-=======
-	//hello1
->>>>>>> refs/heads/master
 
 }
